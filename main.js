201af7ebb431e7ece191962af9d5cd4b3f119c2c
Abdalrahman_Gamal <ghozalabdo456@gmail.com>
2025-04-19 21:03:38 +0200

    menu icon & rest of pages with some js

--- main.js
+++ main.js
@@ -1,15 +1,48 @@
-/* General Info */
+/* Sidebar Open-Close */
+let arrowbtn = document.querySelector(".menu-arrow");
+
+if(arrowbtn){ // Validation
+  arrowbtn.addEventListener("click" , (event) => {
+    arrowbtn.parentElement.classList.toggle("menu-closed");
+  })
+}
+/* =============================== */
+
+
+/* Settings General Info */
 let changeField = document.querySelector("#general-info .email input");
 let changeBtn = document.querySelector("#general-info .email a");
 let noChange = true;
 
-changeBtn.onclick = (e) => {
-  if(noChange){
-    e.preventDefault();
-    changeField.removeAttribute("disabled");
-    changeBtn.innerHTML = "Save";
-    noChange = false;
-  }
+if(changeBtn) { // Validation
+  changeBtn.addEventListener("click" , (e) => {
+    if(noChange){
+      e.preventDefault();
+      changeField.removeAttribute("disabled");
+      changeBtn.innerHTML = "Save";
+      noChange = false;
+    }
+  });
+}
+/* ================================== */
+
+
+/* Plans */
+let plans = document.querySelectorAll("#plans-page .plan");
+let planbtns = document.querySelectorAll("#plans-page .plan button");
+
+if(planbtns){ // Validation
+  planbtns.forEach((planbnt) => {
+    planbnt.addEventListener("click" , (e) => {
+      removeJoinedClass();
+      planbnt.parentElement.classList.add("joined"); // Make the plan Joined
+    })
+  });
 }
 
-/* ================================== */
+
+function removeJoinedClass() {
+  planbtns.forEach((planbnt) => {
+    planbnt.parentElement.classList.remove("joined"); // Make all the Classes Unjoined
+  })
+}
